ac27477f2c3766158ef8c39390329c3a3b2a9155
kolobkevic <kolobkevic@google.com>
2022-06-21 20:55:33 +0300

    Changed icon type of the weather to emoji. Current time does not show now.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,12 +1,9 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
-import java.net.URL;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -25,29 +22,17 @@ public class Bot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             SendMessage message = new SendMessage();
             message.setChatId(update.getMessage().getChatId().toString());
-            SendPhoto photo = null;
-            Weather weather = new Weather();
             try {
-                message.setText(weather.getWeather(update.getMessage().getText()));
-                photo = sendImageFromUrl(weather.getPictureURL(), update.getMessage().getChatId().toString());
+                message.setText(new Weather().getWeather(update.getMessage().getText()));
             } catch (IOException e) {
                 message.setText("Город не найден");
             }
 
             try {
                 execute(message);
-                execute(photo);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
     }
-
-    public SendPhoto sendImageFromUrl(String url, String chatId) {
-        SendPhoto sendPhotoRequest = new SendPhoto();
-        sendPhotoRequest.setChatId(chatId);
-        sendPhotoRequest.setPhoto(new InputFile(url));
-        return sendPhotoRequest;
-    }
-
 }
